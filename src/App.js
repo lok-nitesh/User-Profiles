1039c9ccf86f3d6ed58c41d4d01883af97aaffc8
lok-nitesh <loknitesh.dadi@gmail.com>
2024-06-23 13:53:21 +0000

    initial

---
+++ src/App.js
@@ -0,0 +1,8 @@
+const userDetails = {
+  imageURL: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Rahul',
+  role: 'software developer',
+}
+const App = () => <h1>Users List App</h1>
+
+export default App
